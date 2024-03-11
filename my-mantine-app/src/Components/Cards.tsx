7deeb6d1e9690a9a84d5dbf8cb87f77d3305c1d7
Birhanu <birhanuasmamaw24@gmail.com>
2024-03-11 01:27:05 -0900

    Here added a card on the Cards Component

---
+++ my-mantine-app/src/Components/Cards.tsx
@@ -0,0 +1,15 @@
+import {Grid} from "@mantine/core";
+function Cards() {
+  return (
+    <div className='App'>
+      <Grid>
+      <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>1</Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>2</Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>3</Grid.Col>
+      <Grid.Col span={{ base: 12, md: 6, lg: 3 }}>4</Grid.Col>
+    </Grid>
+    </div>
+  );
+}
+
+export default Cards;
